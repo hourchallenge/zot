26a4de0320822b46513fdb1db4bedd088a6a3438
Ben Morris <ben@bendmorris.com>
2012-12-07 13:11:37 -0500

    Use ConfigParser in settings.py to get zotero directory.

--- settings.py
+++ settings.py
@@ -1,1 +1,22 @@
-zotero_dir = '/home/ben/Dropbox/Zotero/'
+import argparse
+import ConfigParser
+import os
+import sys
+
+
+def make_dir_if_not_exists(path):
+    if not os.path.exists(path): os.makedirs(path)
+
+home_dir = os.path.expanduser('~/.zot/')
+make_dir_if_not_exists(home_dir)
+config_file_path = os.path.join(home_dir, 'settings')
+
+
+config = ConfigParser.SafeConfigParser()
+if not config.has_section('zot'): config.add_section('zot')
+config.read(config_file_path)
+try:
+    zotero_dir = config.get('zot', 'zotero_dir')
+except ConfigParser.NoOptionError:
+    print '''No Zotero directory defined. Run 'zotero path [zotero_dir]' to set your Zotero directory.'''
+    sys.exit()

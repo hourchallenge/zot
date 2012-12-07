dbdbe162a52fb89931c1287e5abb857653d4de29
Ben Morris <ben@bendmorris.com>
2012-12-07 14:37:34 -0500

    Enabling zot read.

--- item.py
+++ item.py
@@ -1,4 +1,5 @@
 import os
+import pdf2txt
 
 def format_author(author):
     return '%s, %s' % author
@@ -49,13 +50,15 @@ class Item:
         bib = self.author_string()
         if hasattr(self, 'year'): bib += ' %s.' % self.year
         if hasattr(self, 'title'): bib += ' %s.' % self.title
-        if hasattr(self, 'publicationTitle'): bib += ' %s.' % self.publicationTitle
+        if hasattr(self, 'publicationTitle'): bib += ' %s' % self.publicationTitle
 
         v = []
         if hasattr(self, 'volume'): v += ['Vol. %s' % self.volume]
         if hasattr(self, 'issue'): v += ['Issue %s' % self.issue]
         if hasattr(self, 'pages'): v += ['Pages %s' % self.pages]       
-        if v: bib += ' ' + (', '.join(v)) + '.'
+        if v: bib += ' ' + (', '.join(v))
+        
+        if v or hasattr(self, 'publicationTitle'): bib += '.'
         
         if hasattr(self, 'doi'): bib += ' doi:%s' % self.doi
         
@@ -73,11 +76,10 @@ class Item:
         return citation
         
         
-    def full_text(self, storage_dir):
+    def get_full_text(self, storage_dir):
         if hasattr(self, 'attachments'):
             for attachment in self.attachments:
                 # TODO: read text from pdf
-                return attachment.replace('storage:', os.path.join(storage_dir, self.key + '/'))
-                pass
+                pdf2txt.main(['pdf2txt', attachment.replace('storage:', os.path.join(storage_dir, self.key + '/'))])
         else: 
             return "No PDF attachments."
--- zot.py
+++ zot.py
@@ -156,15 +156,15 @@ def main():
         result = z.search(best=command=='best', **search_args)
         for i in result:
             print getattr(i, 'key')
-    elif command == ('bib', 'bibliography'):
+    elif command in ('bib', 'bibliography'):
         for result in [z.all_items[key].bibliography() for key in args]:
             print result
     elif command in ('cit', 'cite', 'citation'):
         for result in [z.all_items[key].citation() for key in args]:
             print result
     elif command == 'read':
-        for result in [z.all_items[key].full_text(z.zotero_storage_path) for key in args]:
-            print result
+        for key in args:
+            z.all_items[key].get_full_text(z.zotero_storage_path)
     elif command == 'debug':
         for i in [item.__dict__ for item in z.all_items.values()]: print i
     else:
